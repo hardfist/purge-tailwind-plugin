b1c1eca3d8d9a53c4b7606c09697e5e931231890
hardfist <yangjianzju@gmail.com>
2024-10-16 23:59:11 +1100

    chore: init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import '../other/index';
+function render(){
+    console.log('xx');
+    return `h1 class="text-3xl text-5xl font-bold underline">
+    Hello world!
+  </h1>`
+}
+render();
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const { plugin } = require('postcss');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    require('tailwindcss'),
+                                    require('autoprefixer'),
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
